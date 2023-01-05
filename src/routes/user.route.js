ca7748662cb3428a3f0a71fcc7940a238fb436ef
Vincent Zerbib <contact@zelabstudio.com>
2023-01-05 11:10:47 +0100

    save work

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
+const verifyToken = require('../middlewares/veryfyToken');
 
-router.get('/', userController.getUsers);
-router.get('/:id', userController.getOneUser);
+router.get('/users', userController.getUsers);
+router.get('/:id', verifyToken, userController.getOneUser);
 router.post('/', userController.register);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteOneUser)
